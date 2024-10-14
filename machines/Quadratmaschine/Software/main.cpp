1572df2b99f00bf48b78ee3ff39d25fc5866bf11
Marcel Siegwart <marcel.siegwart@gmx.ch>
2024-10-14 12:40:14 +0200

    bugfix Makefile

--- machines/Quadratmaschine/Software/main.cpp
+++ machines/Quadratmaschine/Software/main.cpp
@@ -2,13 +2,8 @@
 #include <sys/mount.h>
 #include "QuadratMachine.h"
 
-
-
-
 int main(int argc, char* argv[])
 {
-   
-#if 0
     int handle = -1;
     if (argc == 2)
     {
@@ -24,29 +19,10 @@ int main(int argc, char* argv[])
 
     fprintf(stderr,"mi quadrat machine\n");
     
-    handle = ::open("/dev/sda1", O_RDWR);
-    if (handle == -1)
-    {
-        printf("quadrat machine failed -> no usb\n");
-    }
-    ::close(handle);
-    if (mount("/dev/sda1", "/home/root/sounds", "vfat", MS_NOATIME, NULL)) {
-        if (errno == EBUSY) {
-            printf("quadrat machine Mountpoint busy\n");
-            return 1;
-        }
-        else {
-            printf("quadrat machine Mount error: %s\n", strerror(errno));
-            return 1;
-        }
-    }
-    else {
-        printf("miwoodenmachine Mount successful\n");
-    }
-#endif
     const std::string _WavePath = std::string("/home/root/sounds");
     miQuadratMachine::QuadratMachine _QuadratMachine(_WavePath,"/home/root/quadrat.ini");
     _QuadratMachine.start();
+
     while (true)
     {
         usleep(100000);
